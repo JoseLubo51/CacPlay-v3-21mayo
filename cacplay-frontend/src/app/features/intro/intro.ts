8a8d9ba4956d8953c098c6e756e0777ff61f97e6
JoseLubo <tjlubo_51@hotmail.com>
2026-05-21 11:38:06 -0500

    Feat: Implementación de splash screen animada con video y ajustes responsive en intro

--- cacplay-frontend/src/app/features/intro/intro.ts
+++ cacplay-frontend/src/app/features/intro/intro.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -8,14 +8,35 @@ import { Router } from '@angular/router';
   imports: [CommonModule],
   templateUrl: './intro.html'
 })
-export class Intro {
+export class Intro implements OnInit, AfterViewInit {
+  
+  videoActivo: boolean = true;
+
+  // Capturamos el elemento #videoSplash del HTML
+  @ViewChild('videoSplash') videoElement!: ElementRef<HTMLVideoElement>;
 
   constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  // Se ejecuta automáticamente cuando Angular termina de renderizar el DOM
+  ngAfterViewInit(): void {
+    if (this.videoElement && this.videoElement.nativeElement) {
+      // Forzamos la reproducción inmediata controlando la promesa del navegador
+      this.videoElement.nativeElement.play().catch(error => {
+        console.log("El navegador bloqueó el autoplay, esperando interacción:", error);
+      });
+    }
+  }
+
+  onVideoTerminado(): void {
+    this.videoActivo = false;
+  }
 
   // 1. Redirección a la URL externa de inicio de sesión de CAC
   onLoginSso() {
     window.location.href = 'https://cuentadealtocosto.org/iniciar-sesion/'; 
-   
   }
 
   // 2. Redirección al login interno de CACPlay para superadministradores
